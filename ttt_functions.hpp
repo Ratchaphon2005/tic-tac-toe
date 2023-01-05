10bc4cf16cf19f1c3c3f92a211791c116ad7c1c1
Ratchapon Klinprathum <58630777+Ratchaphon2005@users.noreply.github.com>
2023-01-05 12:55:04 +0700

    Update ttt_functions.hpp

--- ttt_functions.hpp
+++ ttt_functions.hpp
@@ -1,7 +1,6 @@
 #include <vector>
-void draw(std::vector<int> player_a, std::vector<int> player_b);
 
+void draw(std::vector<int> player_a, std::vector<int> player_b);
 bool check_win(std::vector<int> player);
 bool check_position_available(std::vector<int> player_a, std::vector<int> player_b, int position);
-
 int check_is_number_1_9(std::string check);
